061a800ba1b9722b0a6c23786a8a11592d3369e6
mappazinho <mappazinho@gmail.com>
2026-09-14 21:56:57 +0300

    clean up deferred SDK 2013 precache compatibility wrapper

--- src/game/client/precache_register.h
+++ src/game/client/precache_register.h
@@ -9,14 +9,4 @@
 
 #include "clienteffectprecachesystem.h"
 
-// viewrender_deferred.cpp includes these immediately after precache_register.h.
-// Pull them in before the compatibility macros below so declarations in the
-// native SDK headers are not affected by the local method-name shims.
-#include "rendertexture.h"
-#include "viewpostprocess.h"
-#include "viewdebug.h"
-#include "deferred/deferred_shared_common.h"
-
-#include "deferred/sdk2013_viewrender_compat.h"
-
 #endif // SDK2013_PRECACHE_REGISTER_COMPAT_H
