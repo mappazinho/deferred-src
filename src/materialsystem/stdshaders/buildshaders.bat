c4ac466ed2c980db3182a579578caaf12118f7d8
mappazinho <mappazinho@gmail.com>
2026-09-15 19:10:37 +0300

    Use standalone SCell555 ShaderCompile pipeline

--- src/materialsystem/stdshaders/buildshaders.bat
+++ src/materialsystem/stdshaders/buildshaders.bat
@@ -14,40 +14,28 @@ echo ==================== buildshaders %* ==================
 set tt_start=%ERRORLEVEL%
 set tt_chkpt=%tt_start%
 
-
 REM ****************
 REM usage: buildshaders <shaderProjectName>
 REM ****************
 
 setlocal
 set arg_filename=%1
-set shadercompilecommand=shadercompile.exe
+set shadercompilecommand=ShaderCompile.exe
 set targetdir=shaders
 set SrcDirBase=..\..
 set shaderDir=shaders
-set SDKArgs=
-set SHADERINCPATH=vshtmp9/... fxctmp9/...
-
 
 if "%1" == "" goto usage
 set inputbase=%1
 
-set DIRECTX_SDK_VER=pc09.00
-set DIRECTX_SDK_BIN_DIR=dx9sdk\utilities
+REM SCell555/ShaderCompile does not use the stock DXSDK selector.
+if /i "%6" == "-dx9_30" shift /6
 
-if /i "%6" == "-dx9_30" goto dx_sdk_dx9_30
-goto dx_sdk_end
-:dx_sdk_dx9_30
-			set DIRECTX_SDK_VER=pc09.30
-			set DIRECTX_SDK_BIN_DIR=dx10sdk\utilities\dx9_30
-			goto dx_sdk_end
-:dx_sdk_end
-
-if /i "%7" == "-force30" goto set_force30_arg
+if /i "%6" == "-force30" goto set_force30_arg
 goto set_force_end
 :set_force30_arg
-			set DIRECTX_FORCE_MODEL=30
-			goto set_force_end
+set IS30=1
+goto set_force_end
 :set_force_end
 
 if /i "%2" == "-game" goto set_mod_args
@@ -63,45 +51,56 @@ echo "       gameDir is where gameinfo.txt is (where it will store the compiled 
 echo "       sourceDir is where the source code is (where it will find scripts and compilers)."
 echo "ex   : buildshaders myshaders"
 echo "ex   : buildshaders myshaders -game c:\steam\steamapps\sourcemods\mymod -source c:\mymod\src"
-goto :end
+goto end
 
 REM ****************
-REM MOD ARGS - look for -game or the vproject environment variable
+REM MOD ARGS
 REM ****************
 :set_mod_args
 
-if not exist "%SDKBINDIR%\shadercompile.exe" goto NoShaderCompile
-set ChangeToDir=%SDKBINDIR%
-
 if /i "%4" NEQ "-source" goto NoSourceDirSpecified
 set SrcDirBase=%~5
+set ChangeToDir=%SrcDirBase%\devtools\bin
+
+REM Install the standalone compiler on first use instead of relying on
+REM Source SDK Base's stock shadercompile.exe in %%SDKBINDIR%%.
+if not exist "%ChangeToDir%\ShaderCompile.exe" (
+    echo SCell555 ShaderCompile is not installed; downloading the pinned release...
+    powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%ChangeToDir%\install_shadercompile.ps1"
+    if errorlevel 1 goto ShaderCompileInstallFailed
+)
+if not exist "%ChangeToDir%\ShaderCompile.exe" goto NoShaderCompile
 
 REM ** use the -game parameter to tell us where to put the files
 set targetdir=%~3\shaders
-set SDKArgs=-nompi -nop4 -game "%~3"
 
 if not exist "%~3\gameinfo.txt" goto InvalidGameDirectory
+if not exist "%inputbase%.txt" goto InvalidInputFile
 goto build_shaders
 
 REM ****************
 REM ERRORS
 REM ****************
 :InvalidGameDirectory
-echo -
 echo Error: "%~3" is not a valid game directory.
 echo (The -game directory must have a gameinfo.txt file)
-echo -
+goto end
+
+:InvalidInputFile
+echo Error: "%inputbase%.txt" is not a valid file.
 goto end
 
 :NoSourceDirSpecified
 echo ERROR: If you specify -game on the command line, you must specify -source.
 goto usage
+
+:ShaderCompileInstallFailed
+echo ERROR: Failed to install SCell555 ShaderCompile.
+echo Run "%SrcDirBase%\devtools\bin\install_shadercompile.ps1" manually for details.
 goto end
 
 :NoShaderCompile
-echo -
-echo - ERROR: shadercompile.exe doesn't exist in %SDKBINDIR%
-echo -
+echo ERROR: ShaderCompile.exe doesn't exist in %SrcDirBase%\devtools\bin
 goto end
 
 REM ****************
@@ -109,101 +108,41 @@ REM BUILD SHADERS
 REM ****************
 :build_shaders
 
-rem echo --------------------------------
-rem echo %inputbase%
-rem echo --------------------------------
-REM make sure that target dirs exist
-REM files will be built in these targets and copied to their final destination
+if not exist include mkdir include
 if not exist %shaderDir% mkdir %shaderDir%
 if not exist %shaderDir%\fxc mkdir %shaderDir%\fxc
-if not exist %shaderDir%\vsh mkdir %shaderDir%\vsh
-if not exist %shaderDir%\psh mkdir %shaderDir%\psh
-REM Nuke some files that we will add to later.
-if exist filelist.txt del /f /q filelist.txt
-if exist filestocopy.txt del /f /q filestocopy.txt
-if exist filelistgen.txt del /f /q filelistgen.txt
-if exist inclist.txt del /f /q inclist.txt
-if exist vcslist.txt del /f /q vcslist.txt
 
-REM ****************
-REM Generate a makefile for the shader project
-REM ****************
-perl "%SrcDirBase%\devtools\bin\updateshaders.pl" -source "%SrcDirBase%" %inputbase%
-
-
-REM ****************
-REM Run the makefile, generating minimal work/build list for fxc files, go ahead and compile vsh and psh files.
-REM ****************
-rem nmake /S /C -f makefile.%inputbase% clean > clean.txt 2>&1
-echo Building inc files, asm vcs files, and VMPI worklist for %inputbase%...
-nmake /S /C -f makefile.%inputbase%
-
-REM ****************
-REM Copy the inc files to their target
-REM ****************
-if exist "inclist.txt" (
-	echo Publishing shader inc files to target...
-	perl %SrcDirBase%\devtools\bin\copyshaderincfiles.pl inclist.txt
+set SHVER=20b
+if defined IS30 (
+    set SHVER=30
 )
 
-REM ****************
-REM Add the executables to the worklist.
-REM ****************
-if /i "%DIRECTX_SDK_VER%" == "pc09.00" (
-	rem echo "Copy extra files for dx 9 std
-)
-if /i "%DIRECTX_SDK_VER%" == "pc09.30" (
-	echo %SrcDirBase%\devtools\bin\d3dx9_33.dll >> filestocopy.txt
-)
+title %1 %SHVER%
 
-echo %SrcDirBase%\%DIRECTX_SDK_BIN_DIR%\dx_proxy.dll >> filestocopy.txt
+echo Building shader headers and VCS files for %inputbase% with SCell555 ShaderCompile...
 
-echo %SDKBINDIR%\shadercompile.exe >> filestocopy.txt
-echo %SDKBINDIR%\shadercompile_dll.dll >> filestocopy.txt
-echo %SDKBINDIR%\vstdlib.dll >> filestocopy.txt
-echo %SDKBINDIR%\tier0.dll >> filestocopy.txt
-
-REM ****************
-REM Cull duplicate entries in work/build list
-REM ****************
-if exist filestocopy.txt type filestocopy.txt | perl "%SrcDirBase%\devtools\bin\uniqifylist.pl" > uniquefilestocopy.txt
-if exist filelistgen.txt if not "%dynamic_shaders%" == "1" (
-    echo Generating action list...
-    copy filelistgen.txt filelist.txt >nul
-)
-
-REM ****************
-REM Execute distributed process on work/build list
-REM ****************
-
-set shader_path_cd=%cd%
-if exist "filelist.txt" if exist "uniquefilestocopy.txt" if not "%dynamic_shaders%" == "1" (
-	echo Running distributed shader compilation...
-
-	cd /D %ChangeToDir%
-	echo %shadercompilecommand% %SDKArgs% -shaderpath "%shader_path_cd:/=\%" -allowdebug
-	%shadercompilecommand% %SDKArgs% -shaderpath "%shader_path_cd:/=\%" -allowdebug
-	cd /D %shader_path_cd%
-)
+set DYNAMIC=
+if "%dynamic_shaders%" == "1" set DYNAMIC=-Dynamic
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%SrcDirBase%\devtools\bin\process_shaders.ps1" %DYNAMIC% -Version %SHVER% "%inputbase%.txt"
+if errorlevel 1 goto ShaderBuildFailed
 
 REM ****************
 REM PC Shader copy
-REM Publish the generated files to the output dir using XCOPY
-REM This batch file may have been invoked standalone or slaved (master does final smart mirror copy)
 REM ****************
 :DoXCopy
 if not "%dynamic_shaders%" == "1" (
-if not exist "%targetdir%" md "%targetdir%"
-if not "%targetdir%"=="%shaderDir%" xcopy %shaderDir%\*.* "%targetdir%" /e /y
+    if not exist "%targetdir%" md "%targetdir%"
+    if not "%targetdir%"=="%shaderDir%" xcopy %shaderDir%\*.* "%targetdir%" /e /y
 )
 goto end
+
+:ShaderBuildFailed
+echo ERROR: Shader compilation failed for %inputbase%.
 
 REM ****************
 REM END
 REM ****************
 :end
 
-
 %TTEXE% -diff %tt_start%
 echo.
-
